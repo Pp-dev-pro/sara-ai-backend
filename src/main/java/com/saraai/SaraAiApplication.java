d006b9bff4cf0f07c820f049962dfbd84dc03ea6
Priyanshu-dev <priyapriyanshu000@gmail.com>
2026-01-13 19:20:17 +0530

    Initialize Spring Boot application structure

---
+++ src/main/java/com/saraai/SaraAiApplication.java
@@ -0,0 +1,11 @@
+package com.saraai;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaraAiApplication {
+  public static void main(String[] args){
+    SpringApplication.run(SaraAiApplication.class, args);
+  }
+}
+
